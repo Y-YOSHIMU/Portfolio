17813bd8c4bfacf3d6c0318db1abfc78e43e99c7
vagrant <vagrant@localhost.localdomain>
2020-08-10 03:20:22 +0000

    [Add]devise&Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+  }
+  devise_for :users, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+  }
+
+  scope module: :public do
+  	root 'home#top'
+  	get 'about' => 'home#about'
+  	# get 'customers/mypage' => 'customers#show', as: 'mypage'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
