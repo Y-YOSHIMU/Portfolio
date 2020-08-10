6cac1335707ffdaf68e368cf310d0cec8460e6de
vagrant <vagrant@localhost.localdomain>
2020-08-10 05:56:01 +0000

    [Add]header_link&mypage

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,16 @@
 class Public::UsersController < ApplicationController
+  	before_action :authenticate_user!
+	before_action :set_current_user
+
+	def show
+	end
 
 	private
+
+	def set_current_user
+    	@user = current_user
+  	end
+
 	def user_params
 		params.require(:user).permit(:nickname)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   scope module: :public do
   	root 'home#top'
   	get 'about' => 'home#about'
-  	# get 'customers/mypage' => 'customers#show', as: 'mypage'
+  	get 'users/mypage' => 'users#show', as: 'mypage'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
