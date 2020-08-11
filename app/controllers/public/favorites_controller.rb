1a6bb8ffb12b4b89fc643cbc263271aeae6fc34f
vagrant <vagrant@localhost.localdomain>
2020-08-11 08:01:22 +0000

    [Add]Ajax_favorites

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,15 @@
 class Public::FavoritesController < ApplicationController
 
 	def create
+		@diary = Diary.find(params[:diary_id])
 		favorite = current_user.favorites.build(diary_id: params[:diary_id])
 		favorite.save
-		redirect_to request.referer
 	end
 
 	def destroy
+		@diary = Diary.find(params[:diary_id])
 		favorite = Favorite.find_by(diary_id: params[:diary_id], user_id: current_user.id)
 		favorite.destroy
-		redirect_to request.referer
 	end
 
 	def index
