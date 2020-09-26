ff2c0eee4d8e8b0092714f78025221f282e467d2
vagrant <vagrant@localhost.localdomain>
2020-09-26 13:16:55 +0000

    [Refactoring]set_diary

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,18 +1,18 @@
 class Public::UsersController < ApplicationController
-  	before_action :authenticate_user!
-	before_action :set_current_user
+    before_action :authenticate_user!
+    before_action :set_current_user
 
-	def show
-		@diaries = @user.diaries
-	end
+  def show
+    @diaries = @user.diaries
+  end
 
-	private
+  private
 
-	def set_current_user
-    	@user = current_user
-  	end
+  def set_current_user
+      @user = current_user
+    end
 
-	def user_params
-		params.require(:user).permit(:nickname)
-	end
+  def user_params
+    params.require(:user).permit(:nickname)
+  end
 end
